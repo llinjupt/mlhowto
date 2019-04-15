c42d804b213fe8abeec64cbe39b198e6e7d3821e
Red Liu <lli_njupt@163.com>
2019-04-15 17:26:28 +0800

    correct minor comment error

--- footstone/crossvalid.py
+++ footstone/crossvalid.py
@@ -68,7 +68,7 @@ def normal_dis_trainset(positive=100, negtive=100, type='normal'):
 def data_split(X, y, ratio=0.3, random_state=0):
     from sklearn.model_selection import train_test_split
     
-    # 'X_train, X_test, y_train, y_test = '
+    # 'X_train, y_test, x_labels, y_labels = '
     return train_test_split(X, y, test_size=ratio, random_state=random_state)
 
 if __name__ == "__main__":
